c20481a985779a0e5fa96f487211553b3ad51b61
Robotechnic <robotechnic.developeur@gmail.com>
2025-07-16 11:04:28 +0300

    changed the database schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -7,11 +7,17 @@ export const projects = pgTable('projects', {
 	revision: integer('revision').notNull().default(0)
 });
 
+export const analyzer = pgTable('analyzer', {
+	id: serial('id').primaryKey(),
+	name: varchar('name', { length: 100 }).notNull().unique(),
+});
+
 export const proofObligation = pgTable('proof_obligation', {
 	id: serial('id').primaryKey(),
 	projectId: integer('project_id').references(() => projects.id).notNull(),
+	projectRevision: integer('project_revision').notNull().default(0),
+	analyzerId: integer('analyzer_id').references(() => analyzer.id).notNull(),
 	revision: integer('revision').notNull().default(0),
-	name: varchar('name', { length: 100 }).notNull(),
 	lastUpdated: date('last_updated').notNull().defaultNow(),
 	proofObligation: json('proof_obligation').notNull(),
 });
@@ -19,7 +25,7 @@ export const proofObligation = pgTable('proof_obligation', {
 export const conflict = pgTable('conflict', {
 	id: serial('id').primaryKey(),
 	projectId: integer('project_id').references(() => projects.id).notNull(),
-	revision: integer('revision').notNull().default(0),
+	projectRevision: integer('revision').notNull().default(0),
 	proofObligationId1: integer('proof_obligation_id_1').references(() => proofObligation.id).notNull(),
 	proofObligationId2: integer('proof_obligation_id_2').references(() => proofObligation.id).notNull(),
 	conflicts: json('conflicts').notNull(),

b0c120471c7cde3aa09a6c261ff8a8f4f16c0327
Robotechnic <robotechnic.developeur@gmail.com>
2025-07-18 11:50:44 +0300

    added a proper error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import {page} from '$app/state';
+</script>
+
+<section>
+	<h2>
+		{page.status}
+	</h2>
+	<p>
+		{page.error?.message || 'An unexpected error occurred.'}
+	</p>
+	<p>
+		Back to <a href="/">home</a>.
+	</p>
+</section>
+
+<style lang="scss">
+	section {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+		width: 100%;
+
+		h2 {
+			font-size: 8rem;
+			color: var(--color-error);
+			margin-bottom: 0;
+		}
+
+		p {
+			margin: 0;
+			font-size: 2rem;
+		}
+	}
+</style>

b21ffe0f18c25d1ad7b892f2ba68913d0b937bb4
Robotechnic <robotechnic.developeur@gmail.com>
2025-07-22 15:59:55 +0300

    changed the output format as the source file hash is no longer needed

--- lib/report.ml
+++ lib/report.ml
@@ -1,8 +1,17 @@
 open Ppx_yojson_conv_lib.Yojson_conv.Primitives
 
-type sources = (string, string) Hashtbl.t
-let yojson_of_sources = yojson_of_hashtbl yojson_of_string yojson_of_string
-let sources_of_yojson = hashtbl_of_yojson string_of_yojson string_of_yojson
+type sources = (string, unit) Hashtbl.t
+let yojson_of_sources (h: (string, unit) Hashtbl.t)  = `List (
+  Hashtbl.fold (fun k _ acc -> (`String k) :: acc) h [])
+
+let sources_of_yojson = function
+  | `List lst ->
+    let tbl = Hashtbl.create (List.length lst) in
+    List.iter (function
+      | `String file -> Hashtbl.add tbl file ()
+      | _ -> failwith "Invalid sources format") lst;
+    tbl
+  | _ -> failwith "Expected a list of sources"
 
 type t = {
   mutable conflicts: Conflict.t list;
@@ -19,17 +28,10 @@ let create po1_name po2_name = {
   sources = Hashtbl.create 16;
 }
 
-let extract_source file_path =
-  let ic = open_in file_path in
-  let hash = Sha256.input ic in
-  close_in ic;
-  Sha256.to_hex hash
-
 let add_conflict t files conflicts =
   t.conflicts <- conflicts @ t.conflicts;
   List.iter (fun file ->
-    let content = extract_source file in
-    Hashtbl.add t.sources file content
+    Hashtbl.add t.sources file ()
   ) files
   
   

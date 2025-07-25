7d9ccca1769295b1dd8a3d3a68ad0dca0c1c188d
Robotechnic <robotechnic.developeur@gmail.com>
2025-07-25 15:25:48 +0300

    added a cleaning hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,18 @@
 import type { ServerInit } from '@sveltejs/kit';
 import findRemoveSync from 'find-remove';
 
+function cleanupOldFiles() {
+	console.log('Cleaning up old project files...');
+	findRemoveSync('./projects', { extensions: ['.json'], maxLevel: 1, age: { seconds: 60 * 10 } });
+}
+
 export const init: ServerInit = () => {
 	console.log('Server initialized, setting up cleanup task for old project files.');
 	setInterval(
 		() => {
-			console.log('Cleaning up old project files...');
-			findRemoveSync('./projects/', { age: { seconds: 1000 * 60 * 10 }, extensions: ['.json'] });
+			cleanupOldFiles();
 		},
-		1000 * 60 * 10
+		1000 * 60 * 10 // every 10 minutes
 	);
-	findRemoveSync('./projects/', { age: { seconds: 1000 * 60 * 10 }, extensions: ['.json'] });
+	cleanupOldFiles();
 };

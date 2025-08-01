7e5b9ee749a73988c1cab261b316785b8f94653f
Robotechnic <robotechnic.developeur@gmail.com>
2025-08-01 11:14:16 +0300

    fixed bugs in the reducer

--- reducer.sh
+++ reducer.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 FILE_NAME="$(pwd)/<path>"
-DASHBOARD_PATH="<dashbard>"
+DASHBOARD_PATH="<dashbard"
+MIN_ERR_CODE=4
 
 echo "Running analysis on $FILE_NAME" 1>&2
+
+gcc -fsyntax-only "$FILE_NAME" -std=c11
+if [ $? -ne 0 ]; then
+	echo "GCC syntax check failed"
+	exit 1
+fi
 
 goblint --ana.arrayoob true --ana.int.interval_set true --ana.float.interval true --ana.float.evaluate_math_functions true --ana.base.arrays.domain trivial --ana.base.arrays.nullbytes true --ana.base.strings.domain disjoint --sem.malloc.fail true --set "ana.activated[+]" memOutOfBounds --set "ana.activated[+]"  useAfterFree --warn.quote-code true  --dbg.timing.enabled true --result dashboard  --ana.sv-comp.functions true  --warn.error false --warn.warning false --warn.info false \
  --outfile out1.json "$FILE_NAME"
@@ -13,7 +20,7 @@ if [ $? -ne 0 ]; then
 fi
 
 mopsa-c -silent /home/robotechnic/Documents/Annee_2025-2024/stage/_opam/share/mopsa/stubs/c/libc/stdlib.c /home/robotechnic/Documents/Annee_2025-2024/stage/_opam/share/mopsa/stubs/c/libc/assert.c /home/robotechnic/Documents/Annee_2025-2024/stage/_opam/share/mopsa/stubs/c/libc/unistd.c /home/robotechnic/Documents/Annee_2025-2024/stage/_opam/share/mopsa/stubs/c/libc/libintl.c /home/robotechnic/Documents/Annee_2025-2024/stage/_opam/share/mopsa/stubs/c/libc/stdio_ext.c /home/robotechnic/Documents/Annee_2025-2024/stage/_opam/share/mopsa/stubs/c/libc/wctype.c /home/robotechnic/Documents/Annee_2025-2024/stage/_opam/share/mopsa/stubs/c/libc/sys/stat.c /home/robotechnic/Documents/Annee_2025-2024/stage/_opam/share/mopsa/stubs/c/libc/error.c /home/robotechnic/Documents/Annee_2025-2024/stage/_opam/share/mopsa/stubs/c/libc/math.c /home/robotechnic/Documents/Annee_2025-2024/stage/_opam/share/mopsa/stubs/c/libc/stdio.c /home/robotechnic/Documents/Annee_2025-2024/stage/_opam/share/mopsa/stubs/c/libc/stdlib.c\
- -output ./out2.json -format=json \
+ -output ./out2.json -format=json -show-safe-checks \
  "$FILE_NAME"
 if [ $? -ne 0 ]; then
 	echo "Mopsa analysis failed"
@@ -21,6 +28,7 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
+echo "Running dashboard analysis on $FILE_NAME" 1>&2
 $DASHBOARD_PATH \
 	--analyze "$FILE_NAME" \
 	"out1.json" \
@@ -29,13 +37,14 @@ $DASHBOARD_PATH \
 result=$?
 
 if [ $result -eq 1 ]; then
-	echo "Dashboard analysis failed"
+	echo "Dashboard analysis failed" 1>&2
 	exit 1
 fi
 
-# We exit code to be 4 (i.e safety conflicts)
-if [ $result -eq 4 ]; then
+# We exit code to be 4 (i.e safety conflicts) or 5 (i.e error level conflicts)
+if [ $result -ge $MIN_ERR_CODE ]; then
 	exit 0
 else
+	echo "Expected result to be at least $MIN_ERR_CODE, but got $result" 1>&2
 	exit 1
 fi

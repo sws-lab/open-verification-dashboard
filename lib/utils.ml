6fb7db65f80e467f62ecdf2e61808c79d7782ec7
Robotechnic <robotechnic.developeur@gmail.com>
2026-01-15 15:34:28 +0100

    fixed a typo

--- lib/utils.ml
+++ lib/utils.ml
@@ -4,7 +4,7 @@ let string_yojson_of_t yojson_of_t kind =
   | _ -> failwith "Expected a list"
 (**
   Utility function to convert a JSON representation of the form [`List [`String s]] to [`String s].
-  This is useful for converting JSON objects that are outputted as lists by yojson_ppm into the expected string format.
+  This is useful for converting JSON objects that are outputted as lists by yojson_ppx into the expected string format.
 *)
 
 let string_t_of_yojson t_of_yojson name json =
@@ -15,5 +15,5 @@ let string_t_of_yojson t_of_yojson name json =
     failwith ("Expected a string value for " ^ name ^ ", got: " ^ Yojson.Safe.to_string json)
 (**
   Utility function to convert a JSON representation of the form [`String s] to a form [`List [`String s]].
-  This is useful for converting JSON objects that are expected to be a single string in the file into the expected yojson_ppm list format.
+  This is useful for converting JSON objects that are expected to be a single string in the file into the expected yojson_ppx list format.
 *)

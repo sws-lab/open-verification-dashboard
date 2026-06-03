417ea44181f8efb91e3c6332df3e624166baf86b
Simmo Saan <simmo.saan@gmail.com>
2026-06-03 16:00:25 +0300

    Add ECOOP 2026 artifact quickstart as tests

---
+++ test/compare-artifact.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+OUTDIR=dashboard-artifact-regression/test7
+
+mkdir -p $OUTDIR
+
+./run-dashboard-comparisons.py --mopsa dashboard-artifact-data/quickstart/tool-outputs/mopsa.*.files/SV-COMP26_no-overflow/ --goblint dashboard-artifact-data/quickstart/tool-outputs/goblint.*.files/SV-COMP26_no-overflow/ --dashboard $PWD/.. --analyze '/home/holterka/dashboard/dashboard-artifact-data/sv-benchmarks-nooverflow/c/**' --out $OUTDIR/quickstart --log-file $OUTDIR/quickstart.log --progress-every 1
+
+./run-dashboard-comparisons.py --mopsa dashboard-artifact-data/RQ1/tool-outputs-before/mopsa.*.files/SV-COMP26_no-overflow/ --goblint dashboard-artifact-data/RQ1/tool-outputs-before/goblint.*.files/SV-COMP26_no-overflow/ --dashboard $PWD/.. --analyze '/home/holterka/sv-benchmarks/c/**' --out $OUTDIR/RQ1-before --log-file $OUTDIR/RQ1-before.log --progress-every 1000
+./run-dashboard-comparisons.py --mopsa dashboard-artifact-data/RQ1/tool-outputs/mopsa.*.files/SV-COMP26_no-overflow/ --goblint dashboard-artifact-data/RQ1/tool-outputs/goblint.*.files/SV-COMP26_no-overflow/ --dashboard $PWD/.. --analyze '/home/holterka/sv-benchmarks/c/**' --out $OUTDIR/RQ1 --log-file $OUTDIR/RQ1.log --progress-every 1000
+
+./run-dashboard-comparisons.py --mopsa dashboard-artifact-data/RQ2/tool-outputs/mopsa.*.files/SV-COMP26_no-overflow/ --goblint dashboard-artifact-data/RQ2/tool-outputs/goblint.*.files/SV-COMP26_no-overflow/ --dashboard $PWD/.. --analyze '/home/holterka/sv-benchmarks/c/**' --out $OUTDIR/RQ2 --log-file $OUTDIR/RQ2.log --progress-every 1
